bbba6116d79285c22a1504c24c2ccbb7cdfdeb8e
persey86 <persey86@ukr.net>
2017-03-22 15:02:05 +0200

    creates Exceptions class

--- src/main/java/com/border/servlet/actions/Action.java
+++ src/main/java/com/border/servlet/actions/Action.java
@@ -4,7 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Created by Anastasia on 19.03.2017.
+ * Created on 19.03.2017.
  */
 public interface Action {
     String execute(HttpServletRequest request, HttpServletResponse response) throws Exception;
